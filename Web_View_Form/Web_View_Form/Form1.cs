0b7042213d8d45e3fd1fa9be467f691838d0b1f5
吉田快 <alter.ego.qb@gmail.com>
2019-07-16 10:15:02 +0900

    Enable Argument

--- Web_View_Form/Web_View_Form/Form1.cs
+++ Web_View_Form/Web_View_Form/Form1.cs
@@ -13,15 +13,19 @@ namespace Web_View_Form
 {
     public partial class Form1 : Form
     {
+
+        public static string override_address = "null";
         public Form1()
         {
             InitializeComponent();
             string CD = System.Environment.CurrentDirectory;
 
-           
+
 
             //string address = @"https://www.youtube.com/watch?v=wP8OmKx3ds4";
             string address = CD + @"\data\index.html";
+
+            if (override_address != "null") address = override_address;
 
 
             var browser = new ChromiumWebBrowser(address);
--- Web_View_Form/Web_View_Form/Program.cs
+++ Web_View_Form/Web_View_Form/Program.cs
@@ -12,8 +12,11 @@ namespace Web_View_Form
         /// アプリケーションのメイン エントリ ポイントです。
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
+            if (args.Length == 1) Form1.override_address = args[0];
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
